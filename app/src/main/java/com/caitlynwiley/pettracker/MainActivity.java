6855bf1a91b90fe8a1a9fd44f2e62d5ce28c3a67
Caitlyn Wiley <cwiley982@gmail.com>
2018-06-26 10:05:53 -0400

    Fixed some lint errors

--- app/src/main/java/com/caitlynwiley/pettracker/MainActivity.java
+++ app/src/main/java/com/caitlynwiley/pettracker/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -12,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
