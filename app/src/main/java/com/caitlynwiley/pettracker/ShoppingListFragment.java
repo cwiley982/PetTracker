27bfd0c7bc288034a7e28b509b289f38835604e0
Caitlyn Wiley <cwiley982@gmail.com>
2018-06-26 15:46:21 -0400

    Added animation for fab

--- app/src/main/java/com/caitlynwiley/pettracker/ShoppingListFragment.java
+++ app/src/main/java/com/caitlynwiley/pettracker/ShoppingListFragment.java
@@ -8,6 +8,8 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 
@@ -17,29 +19,44 @@ import java.util.List;
 public class ShoppingListFragment extends ListFragment {
 
     List mList;
-    ArrayAdapter mAdadpter;
+    ArrayAdapter mAdapter;
     FloatingActionButton fab;
+    Animation mRotateForward;
+    Animation mRotateBackward;
+    View mFragView;
+
+    boolean fabOpen;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mList = new ArrayList();
-        fab = getView().findViewById(R.id.shopping_list_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // open a dialog that allows user to type in an item?
-                // Maybe just add a new row to the list that they can type in
-            }
-        });
-        mAdadpter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_checked, mList);
-        mAdadpter.setNotifyOnChange(true);
-        setListAdapter(mAdadpter);
+        mAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_checked, mList);
+        mAdapter.setNotifyOnChange(true);
+        setListAdapter(mAdapter);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.shopping_list_fragment, container, false);
+        mFragView = inflater.inflate(R.layout.shopping_list_fragment, container, false);
+        mRotateForward = AnimationUtils.loadAnimation(getContext(), R.anim.fab_spin_forward);
+        mRotateBackward = AnimationUtils.loadAnimation(getContext(), R.anim.fab_spin_backward);
+        fab = mFragView.findViewById(R.id.shopping_list_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (fabOpen) {
+                    fab.startAnimation(mRotateBackward);
+                } else {
+                    fab.startAnimation(mRotateForward);
+                }
+                fabOpen = !fabOpen;
+                // open a dialog that allows user to type in an item?
+                // Maybe just add a new row to the list that they can type in
+            }
+        });
+        fabOpen = false;
+        return mFragView;
     }
 }
