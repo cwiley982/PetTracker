dc21894f146f5cf52f220d0eb1d1d909e8001b8d
Caitlyn Wiley <cwiley982@gmail.com>
2019-01-25 13:00:43 -0500

    Working on storing events under an account

--- app/src/main/java/com/caitlynwiley/pettracker/LoginActivity.java
+++ app/src/main/java/com/caitlynwiley/pettracker/LoginActivity.java
@@ -105,29 +105,6 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.email_sign_in_button:
-
-                /*
-                String email = mEmailView.getText().toString();
-                String password = mPasswordView.getText().toString();
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "signInWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    updateUI(user);
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                    updateUI(null);
-                                }
-                            }
-                        });
-                        */
         }
     }
 }
