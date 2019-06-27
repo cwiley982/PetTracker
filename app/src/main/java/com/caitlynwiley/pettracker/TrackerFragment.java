4a76f7d9c738294f06993680d30f1daadc91be09
cwiley982 <cwiley982@gmail.com>
2019-06-27 15:23:18 -0400

    Add import to fix error

--- app/src/main/java/com/caitlynwiley/pettracker/TrackerFragment.java
+++ app/src/main/java/com/caitlynwiley/pettracker/TrackerFragment.java
@@ -31,6 +31,7 @@ import java.util.Locale;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
