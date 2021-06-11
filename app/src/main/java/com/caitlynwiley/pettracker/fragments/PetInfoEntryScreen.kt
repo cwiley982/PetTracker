8fc84ced7bd3d4c811407d6617b95bb6c8ab815a
Caitlyn Wiley <cwiley@republicwireless.com>
2021-06-11 14:13:03 -0400

    Migrating to Jetpack Compose
    Also updated several dependencies

---
+++ app/src/main/java/com/caitlynwiley/pettracker/fragments/PetInfoEntryScreen.kt
@@ -0,0 +1,9 @@
+package com.caitlynwiley.pettracker.fragments
+
+import androidx.compose.runtime.Composable
+import com.caitlynwiley.pettracker.kotlincompose.PetTrackerViewModel
+
+@Composable
+fun PetInfoEntryScreen(viewModel: PetTrackerViewModel) {
+
+}
