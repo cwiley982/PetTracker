e98d5657543ccfc21430bb262a8f2c72359cb37d
Caitlyn Wiley <cwiley@republicwireless.com>
2021-06-29 20:33:01 -0400

    Updated CreateAccountActivity to use jetpack compose

--- app/src/main/java/com/caitlynwiley/pettracker/activities/CreateAccountActivity.kt
+++ app/src/main/java/com/caitlynwiley/pettracker/activities/CreateAccountActivity.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.caitlynwiley.pettracker.PetTrackerTheme
@@ -90,15 +92,28 @@ class CreateAccountActivity: BaseActivity() {
         var password by remember { mutableStateOf("") }
         var passwordAgain by remember { mutableStateOf("") }
 
-        Column(modifier = Modifier.padding(8.dp)) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)) {
             Text("Email", modifier = Modifier.padding(4.dp))
-            TextField(value = email, onValueChange = { email = it })
+            TextField(value = email, modifier = Modifier.fillMaxWidth(),
+                onValueChange = { email = it })
 
             Text("Enter your password", modifier = Modifier.padding(4.dp))
-            TextField(value = password, onValueChange = { password = it })
+            TextField(
+                value = password,
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = { password = it },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+            )
 
             Text("Enter your password again", modifier = Modifier.padding(4.dp))
-            TextField(value = passwordAgain, onValueChange = { passwordAgain = it })
+            TextField(
+                value = passwordAgain,
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = { passwordAgain = it },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+            )
 
             Button(
                 onClick = { onSubmit(email, password, passwordAgain) }, modifier = Modifier
